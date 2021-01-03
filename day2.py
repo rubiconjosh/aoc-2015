149979cfcc1c672cc810548b0af52180b61e3bac
Josh Grant <rubiconjosh@gmail.com>
2021-01-02 20:13:29 -0800

    day 2 puzzle 2

--- day2.py
+++ day2.py
@@ -4,6 +4,8 @@ input = input.read()
 lines = input.splitlines()
 
 total = 0
+ribbon = 0
+
 for line in lines:
     dimensions = line.split('x')
     dimensions = [int(x) for x in dimensions]
@@ -11,5 +13,8 @@ for line in lines:
     total += 3 * dimensions[0] * dimensions[1]
     total += 2 * dimensions[0] * dimensions[2]
     total += 2 * dimensions[1] * dimensions[2]
+    ribbon += dimensions[0] * dimensions[1] * dimensions[2]
+    ribbon += 2 * dimensions[0] + 2 * dimensions[1]
 
 print(total)
+print(ribbon)
