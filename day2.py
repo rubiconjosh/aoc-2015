31057e990feb9c1f529366818be504e2aca10d6d
Josh Grant <rubiconjosh@gmail.com>
2021-01-02 20:09:50 -0800

    day 2 puzzle 1

---
+++ day2.py
@@ -0,0 +1,15 @@
+input = open("day2-input.txt")
+input = input.read()
+
+lines = input.splitlines()
+
+total = 0
+for line in lines:
+    dimensions = line.split('x')
+    dimensions = [int(x) for x in dimensions]
+    dimensions.sort()
+    total += 3 * dimensions[0] * dimensions[1]
+    total += 2 * dimensions[0] * dimensions[2]
+    total += 2 * dimensions[1] * dimensions[2]
+
+print(total)
