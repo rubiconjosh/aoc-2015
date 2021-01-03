86671ab522d4757367af9a20e7389cae4dd58940
Josh Grant <rubiconjosh@gmail.com>
2021-01-02 19:01:15 -0800

    day 1 part 2

--- day1.py
+++ day1.py
@@ -1,10 +1,14 @@
 input = open('day1-input.txt')
 input = input.read()
 floor = 0
-for instruction in input:
+positionEnteringBasement = 0
+for position, instruction in enumerate(input):
     if instruction == '(':
         floor += 1
     else:
         floor -= 1
+    if floor < 0 and positionEnteringBasement == 0:
+        positionEnteringBasement = position + 1
 
-print(floor)
+print("Final floor: ", floor)
+print("Position entering basement: ", positionEnteringBasement)
